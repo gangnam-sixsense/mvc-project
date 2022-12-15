d3dba6e09be245effdb2d2c32af0b95c24ee5a21
LSY <lsyool90@gmail.com>
2022-12-15 17:04:59 +0900

    ver1.0-Model

--- sixsense_/src/model/MemberDAO.java
+++ sixsense_/src/model/MemberDAO.java
@@ -1,5 +1,74 @@
 package model;
 
-public class MemberDAO {
+import java.util.ArrayList;
 
+public class MemberDAO {
+	private ArrayList<MemberVO> mvo; // MemberVO를 담고있는 배열.
+
+	public MemberDAO(){ 
+		mvo=new ArrayList<MemberVO>();
+		mvo.add(new MemberVO("sixsense","1234","완벽")); //관리자 정보.
+	}
+	// C
+	public boolean isInputMember(MemberVO vo) { //회원가입.
+		for(int i=0; i<mvo.size(); i++) { //리스트 size만큼 돌아서 
+			if(mvo.get(i).getID().equals(vo.getID())) { //id가 이미 저장된 VO에 있으면 false
+				System.out.println("로그: 아이디 중복이에요.");//개발자만 볼 수 있는 로그
+				return false; // false를 리턴해준다.
+			}
+		}
+		MemberVO data=new MemberVO();  //일회용.
+		data.setID(vo.getID()); // MemberVO의 아이디를 설정해준다.
+		data.setName(vo.getName()); //MemberVO의 이름을 설정해준다.
+		data.setPw(vo.getPw()); //MemberVO의 비밀번호를 설정해준다.
+		mvo.add(vo);
+		System.out.println("로그: 회원가입 성공!"); // 개발자만 볼 수 있는 로그.
+		return true; //같은 아이디가 존재하지 않다면 true 반환 
+		//ㄴ???
+	}
+	//R
+	public MemberVO hasMember(MemberVO vo) {// 로그인 로직
+		for(int i=0;i<mvo.size();i++) { //mvo사이즈만큼 돌아서
+			if(mvo.get(i).getID().equals(vo.getID())) { //아이디가 있다면
+				if(mvo.get(i).getPw().equals(vo.getPw())) { //비밀번호가 있다면
+					System.out.println("로그 : 로그인 성공"); //개발자만 볼 수 있는 로그
+					return mvo.get(i); //그 값을 반환한다.
+				}
+				System.out.println("로그 : 비밀번호가 일치하지 않습니다.");//개발자만 볼 수 있는 로그
+				return null; //null반환
+			}
+		}
+		System.out.println("로그 : 아이디가 일치하지 않습니다.");//개발자만 볼 수 있는 로그
+		return null; //null반환
+	}
+	//U
+	public boolean isModifyMember(MemberVO vo) { //정보변경.
+		for(int i=0; i<mvo.size(); i++) { //리스트 size만큼 돌아서.
+			if(mvo.get(i).getID().equals(vo.getID())) { //id가 있으면.
+				if(vo.getPw()!=null) { //비밀번호가 null이 아니라면.
+					mvo.get(i).setPw(vo.getPw()); //비밀번호를 변경한다.
+					System.out.println("로그: 비밀번호 변경 성공."); //개발자만 볼 수 있는 로그.
+					return true;  //true를 반환한다.
+				}else if(vo.getName()!=null) { // 이름이 null이 아니라면.
+					mvo.get(i).setName(vo.getName()); //이름을 변경한다.
+					System.out.println("로그: 이름 변경 성공."); //개발자만 볼 수 있는 로그.
+					return true; //true를 반환한다.
+				}
+			}
+		}
+		System.out.println("로그: 아이디가 없습니다.(정보변경실팽)"); //개발자만 볼 수 있는 로그.
+		return false; //id가 없다면 false를 반환한다.
+	}
+	//D
+	public boolean isDeleteMember(MemberVO vo) { // 회원 탈퇴.
+		for(int i=0; i<mvo.size(); i++) { //리스트size만큼 돌아서.
+			if(mvo.get(i).getID().equals(vo.getID())) { //비밀번호가 맞다면 if문 실행
+				mvo.remove(i);  //삭제.
+				System.out.println("로그: 회퉌 탈퇴 됐습니다."); //개발자만 볼 수 있는 로그.
+				return true; //true 반환
+			}
+		}
+		System.out.println("로그: 아이디가 존재하지 않습니다.(회원탈퇴)"); //개발자만 볼 수 있는 로그.
+		return false; // 같은 아이디가 존재하지 않다면 false 반환
+	}
 }
--- sixsense_/src/model/MemberVO.java
+++ sixsense_/src/model/MemberVO.java
@@ -1,5 +1,34 @@
 package model;
 
-public class MemberVO {
 
+public class MemberVO {
+	private String ID; //PK
+	private String pw; //비밀번호.
+	private String name; //이름.
+	//ArrayList<ScheduleVO> sArrayList ;
+	public String getID() {
+		return ID;
+	}
+	public void setID(String iD) {
+		ID = iD;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public MemberVO() {	} //기본생성자
+	public MemberVO(String ID, String pw, String name) { //관리자 샘플데이터를 위해 작성.
+		this.ID=ID;
+		this.pw=pw;
+		this.name=name;
+	}
+	
 }
