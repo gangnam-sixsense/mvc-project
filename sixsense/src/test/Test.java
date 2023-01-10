8269c7f7fa59121e959fab3f951705978955af10
min seong <son344910@naver.com>
2023-01-10 23:42:10 +0900

    ㅋ

--- sixsense/src/test/Test.java
+++ sixsense/src/test/Test.java
@@ -3,5 +3,7 @@ package test;
 public class Test {
 public static void main(String[] args) {
 	System.out.println("수정");
+	System.out.println("이것도 못 함?");
+	System.out.println("바");
 }
 }
---
+++ sixsense/src/test/Test02.java
@@ -0,0 +1,7 @@
+package test;
+
+public class Test02 {
+	public static void main(String[] args) {
+		System.out.println("이게 신기해요?");
+	}
+}
