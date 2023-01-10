5306044375c37cfae75d1390d30c1ffeef333d4d
barojmk <jennymin06@gmail.com>
2023-01-10 23:51:46 +0900

    와오아ㅏㅏ

--- sixsense/src/test/Test.java
+++ sixsense/src/test/Test.java
@@ -4,5 +4,6 @@ public class Test {
 public static void main(String[] args) {
 	System.out.println("수정");
 	System.out.println("이것도 못 함?");
+	System.out.println("드디어어ㅓㅓㅓ 된다ㅏㅏ고ㅗ오오오오 뭐라하지마아ㅏ아앙");
 }
 }
